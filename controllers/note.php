58a4f270205ebbdd39b9e5872cd00c0e74265f51
Abdurrahim Ozturk <abdurrahim.ozturk@o2o.be>
2024-12-26 17:38:32 +0100

    Unath and make it more readable

--- controllers/note.php
+++ controllers/note.php
@@ -7,6 +7,18 @@ $heading = " Note";
 
 
 
-$note = $db -> query('SELECT * FROM notes where id = :id', ['id' => $_GET['id']])->fetch();
+$note = $db -> query('SELECT * FROM notes where id = :id', [
+    'id' => $_GET['id']
+])->fetch();
+ 
+if(! $note){
+    abort();
+};
+
+$currentUserId = 1;
+
+if($note['user_id'] != $currentUserId){
+    abort(Response::FORBIDDEN); 
+}
 
 require "views/note.view.php";  
