2f1d37232d3b0a376c325da51359c3a1c3085cfc
Abdurrahim Ozturk <abdurrahim.ozturk@o2o.be>
2024-12-27 18:57:53 +0100

    implement namespace and see

--- Core/Database.php
+++ Core/Database.php
@@ -1,24 +1,29 @@
 <?php
-// connect to the database and execute a query
+
+namespace Core;
+
+use PDO;
 
 class Database
 {
     public $connection;
     public $statement;
-     
-    public function __construct($config, $username = 'root',$password = '')
+
+    public function __construct($config, $username = 'root', $password = '')
     {
-        
         $dsn = 'mysql:' . http_build_query($config, '', ';');
 
-        $this -> connection = new PDO($dsn, $username, $password, [
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        $this->connection = new PDO($dsn, $username, $password, [
+           PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ]);
     }
+
     public function query($query, $params = [])
     {
-        $this -> statement = $this->connection->prepare($query);
-        $this -> statement->execute($params);
+        $this->statement = $this->connection->prepare($query);
+
+        $this->statement->execute($params);
+
         return $this;
     }
 
@@ -31,18 +36,15 @@ class Database
     {
         return $this->statement->fetch();
     }
-    
+
     public function findOrFail()
     {
         $result = $this->find();
-        if(! $result){
+
+        if (! $result) {
             abort();
         }
+
         return $result;
     }
 }
-
-
-
-
-?>
