fb08d739502332266af3da92d5eee837f141c6d2
Abdurrahim Ozturk <abdurrahim.ozturk@o2o.be>
2024-12-31 00:19:55 +0100

    Updating With PATCH Requests

---
+++ controllers/notes/update.php
@@ -0,0 +1,46 @@
+<?php
+
+use Core\Database;
+use Core\App;
+use Core\Validator;
+
+$db = App::resolve(Database::class);
+
+$currentUserId = 1;
+
+$note = $db->query('select * from notes where id = :id', [
+    'id' => $_POST['id']
+])->findOrFail();
+
+authorize($note['user_id'] === $currentUserId);
+//authorize the current user can edit the note
+
+authorize($note['user_id'] === $currentUserId);
+
+// validate the form
+
+$errors = [];
+
+if (! Validator::string($_POST['body'], 1, 1000)) {
+    $errors['body'] = 'A body of no more than 1,000 characters is required.';
+}
+
+// If no validation errors, update the note
+
+if (count($errors)) {
+    return view('notes/edit.view.php', [
+        'heading' => 'Edit Note',
+        'errors' => $errors,
+        'note' => $note
+    ]);
+}
+
+$db->query('update notes set body = :body where id = :id', [
+    'body' => $_POST['body'],
+    'id' => $_POST['id']
+]);
+
+//redirect the user
+
+header('location: /notes');
+die();
