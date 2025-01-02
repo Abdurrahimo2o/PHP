3c164e42ab0a0d3eefa7b255b3103c0466aa44e5
Abdurrahim Ozturk <abdurrahim.ozturk@o2o.be>
2025-01-03 00:23:39 +0100

    Log In and Log Out

--- controllers/registration/store.php
+++ controllers/registration/store.php
@@ -1,54 +1,44 @@
 <?php
 
+use Core\App;
 use Core\Database;
 use Core\Validator;
-use Core\App;
+
+$db = App::resolve(Database::class);
 
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-// validate the form
 $errors = [];
 if (!Validator::email($email)) {
-    $errors['email'] = 'Please enter a valid email address';
+   $errors['email'] = 'Please provide a valid email address.';
 }
+
 if (!Validator::string($password, 7, 255)) {
-    $errors['password'] = 'Please enter a valid password of at least 7 characters';
+    $errors['password'] = 'Please provide a password of at least seven characters.';
 }
 
-if (!empty($errors)) {
+if (! empty($errors)) {
     return view('registration/create.view.php', [
-        'errors' => $errors,
-    ]); 
+        'errors' => $errors
+    ]);
 }
 
-$db = App::resolve(Database::class);
-
-//check if the account already exists
-$user = $db->query('SELECT * FROM users WHERE email = :email',[
+$user = $db->query('select * from users where email = :email', [
     'email' => $email
-]) -> find();
+])->find();
 
-if($user) {
-    // then someone with that email already exists and has an account
-    // If yes, redirect to login page
+if ($user) {
     header('location: /');
     exit();
-    }else {
-        // If not, save the one database, and then log the user in, and redirect  
-        $db->query('INSERT INTO users (email, password) VALUES (:email, :password)', [
-            'email' => $email,
-            'password' => password_hash($password, PASSWORD_BCRYPT),
-        ]);
-    }
-
-    //mark that the user is logged in
-
-    $_SESSION['user'] = [
+} else {
+    $db->query('INSERT INTO users(email, password) VALUES(:email, :password)', [
         'email' => $email,
-    ];
+        'password' => password_hash($password, PASSWORD_BCRYPT)
+    ]);
+
+    login($user);
 
     header('location: /');
     exit();
-
-// view('registration/store.view.php');
+}
